109a81603217b66feffcb5fcf3c3ea2a8cfafe81
Scheinin <scheir3@medtronic.com>
2022-03-04 08:17:48 -0700

    Homework 2

--- js/app.js
+++ js/app.js
@@ -19,4 +19,20 @@ function buildTable (data) {
             }   
         );
     });
+function handleClick () {
+    // Get the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableDate;
+    // Check to see if a date was entered and filter the data using that date.
+    if (date) {
+        // apply 'filter' to the table data to only keep the rows where the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    // Rebuild the table using the filtered data. If no date is entered, then filterData will be the original tableData
+    buildTable(filteredDate);
 }
+// Listen for the form button click
+d3.selectAll("#filter-btn").on("click", handleClick);
+// Build the table when the page loads
+buildTable(tableData);
+};
