3e0924e9818293803568686c362e166efe4ebb1e
Scheinin <scheir3@medtronic.com>
2022-03-04 13:00:45 -0700

    Homework 4

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 // Import the data from data.js
-const tableDate = data;
+const tableData = data;
 
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
@@ -13,16 +13,18 @@ function buildTable (data) {
         // Append a row to the table body
         let row = tbody.append("tr");
         // Loop through each field in the dataRow and add each value as a table cell (td)
-        Object.values(dataRow).forEach((val) =>{
+        Object.values(dataRow).forEach((val) => {
             let cell = row.append("td");
             cell.text(val);
             }   
         );
     });
+}
+
 function handleClick () {
     // Get the datetime value from the filter
     let date = d3.select("#datetime").property("value");
-    let filteredData = tableDate;
+    let filteredData = tableData;
     // Check to see if a date was entered and filter the data using that date.
     if (date) {
         // apply 'filter' to the table data to only keep the rows where the 'datetime' value matches the filter value
@@ -35,4 +37,4 @@ function handleClick () {
 d3.selectAll("#filter-btn").on("click", handleClick);
 // Build the table when the page loads
 buildTable(tableData);
-};
+
